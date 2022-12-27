78363750c9761bec87184a548478638af13c21f3
Anshu Raj <107473845+anshupolestar@users.noreply.github.com>
2022-12-27 16:38:33 +0530

    Update constants.js

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
+const rasa_server_url = "https://de5c-180-151-101-114.in.ngrok.io/webhooks/rest/webhook";
 const sender_id = uuidv4();
